56d3d4822b1d25376419d5def6b3d401b0cbd726
VishalBhagour <vishalbhagour7@gmail.com>
2026-04-07 21:30:55 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -7,9 +7,9 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`❌ MongoDB Error: ${error.message}`);
+    console.error(`MongoDB Error: ${error.message}`);
     process.exit(1);
   }
 };
